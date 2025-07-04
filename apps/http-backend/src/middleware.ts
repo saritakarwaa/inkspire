ffa877bca86658d23fb6bef86da59b1d843b57d0
saritakarwaa <karwasarita672@gmail.com>
2025-07-04 21:29:00 +0530

    remove ts-ignore

--- apps/http-backend/src/middleware.ts
+++ apps/http-backend/src/middleware.ts
@@ -1,18 +1,26 @@
- import { Request,Response,NextFunction } from "express";
- import jwt from "jsonwebtoken"
- import { JWTSECRET } from "@repo/backend-common/config";
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+import { JWTSECRET } from "@repo/backend-common/config";
 
+export interface AuthenticatedRequest extends Request {
+  userId: string;
+}
+export function middleware(req: Request, res: Response, next: NextFunction) {
+  if (!JWTSECRET) {
+    throw new Error("JWTSECRET is not defined");
+  }
 
-export function middleware(req:Request,res:Response,next:NextFunction){
-    const token=req.headers["authorization"] ?? "";
+  const token = req.headers["authorization"] ?? "";
+   if (!token) {
+    res.status(401).json({ message: "Missing token" });
+    return
+  }
 
-    const decoded=jwt.verify(token,JWTSECRET);
-    if(decoded){
-        //@ts-ignore
-        req.userId=decoded.userId;
-        next()
-    }
-    else{
-        res.status(403).json({message:"Unauthorized"})
-    }
+  try {
+    const decoded = jwt.verify(token, JWTSECRET) as { userId: string };
+    (req as AuthenticatedRequest).userId = decoded.userId;
+    next();
+  } catch (err) {
+    res.status(403).json({ message: "Unauthorized" });
+  }
 }

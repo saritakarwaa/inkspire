bae62d0c23a4ea8595e8c092d542a90692f81a27
saritakarwaa <karwasarita672@gmail.com>
2025-07-05 15:01:38 +0530

    add:render urls

--- apps/excalidraw-fe/config.ts
+++ apps/excalidraw-fe/config.ts
@@ -1,2 +1,5 @@
-export const HTTP_BACKEND="http://localhost:5000"
-export const WS_BACKEND="http://localhost:8080"
+export const HTTP_BACKEND =
+  process.env.NEXT_PUBLIC_HTTP_BACKEND || "http://localhost:5000";
+
+export const WS_BACKEND =
+  process.env.NEXT_PUBLIC_WS_BACKEND || "ws://localhost:8080";

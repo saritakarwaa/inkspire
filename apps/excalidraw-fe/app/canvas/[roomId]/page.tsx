6709e4b7bad20db90d0bfc15d1e492e11f80f861
saritakarwaa <karwasarita672@gmail.com>
2025-07-05 13:26:57 +0530

    remove await

--- apps/excalidraw-fe/app/canvas/[roomId]/page.tsx
+++ apps/excalidraw-fe/app/canvas/[roomId]/page.tsx
@@ -1,7 +1,7 @@
 
 import { RoomCanvas } from "@/components/RoomCanvas"
-export default async  function Canvas({params}:{params:{roomId:string}}){
-    const {roomId} =await params
+export default function Canvas({params}:{params:{roomId:string}}){
+    const {roomId} =params
     console.log(roomId)
    return <RoomCanvas roomId={roomId} />
 }
